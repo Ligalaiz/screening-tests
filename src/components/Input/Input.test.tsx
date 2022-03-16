fdb3413c12fbd3c4ca9501f2c441005ccfd7b4f1
Ligalaiz <slavakuz88@gmail.com>
2022-03-16 20:02:10 +0300

    refactor: ST-1 add test for Input component

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Input } from '@components/Input';
 
-const renderComponent = () => render(<Input />);
+const handleChange = jest.fn();
+
+const renderComponent = () =>
+  render(
+    <Input
+      placeholder="What needs to be done?"
+      name="inputForm"
+      type="text"
+      handleChange={handleChange}
+      value="value"
+      testid="inputForm"
+    />,
+  );
 
 describe('Input component', () => {
-  describe('Component not exist', () => {
-    it('inputForm check that the component has not been rendered #smoke', () => {
+  describe('Component exist', () => {
+    it('inputForm check that the component has been rendered #smoke', () => {
       const { queryByTestId } = renderComponent();
       const input = queryByTestId('inputForm');
 
-      expect(input).not.toBeInTheDocument();
+      expect(input).toBeInTheDocument();
+    });
+  });
+
+  describe('Сomponent works', () => {
+    it('todoAddField check change handler was called after type', () => {
+      const { getByTestId } = renderComponent();
+      const input = getByTestId('inputForm');
+
+      userEvent.type(input, 'test');
+      expect(handleChange).toHaveBeenCalledTimes(4);
     });
   });
 });
