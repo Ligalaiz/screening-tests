0d8265f8b1b7ed36bef7818be632f9a450081025
Ligalaiz <slavakuz88@gmail.com>
2022-03-16 15:13:50 +0300

    refactor: ST-1 add tests for Button component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Button } from '@components/Button';
 
-const renderComponent = () => render(<Button />);
+const handleClick = jest.fn();
+
+const renderComponent = () =>
+  render(<Button type="button" handleClick={handleClick} testid="formBtn" />);
 
 describe('Btn component', () => {
-  describe('Component not exist', () => {
-    it('formBtn check that the component has not been rendered #smoke', () => {
+  describe('Component exist', () => {
+    it('formBtn check that the component has been rendered #smoke', () => {
       const { queryByTestId } = renderComponent();
       const btn = queryByTestId('formBtn');
 
-      expect(btn).not.toBeInTheDocument();
+      expect(btn).toBeInTheDocument();
+    });
+  });
+
+  describe('Сomponent works', () => {
+    it('formBtn check click handler was called after click', () => {
+      const { getByTestId } = renderComponent();
+      const btn = getByTestId('formBtn');
+
+      userEvent.click(btn);
+      expect(handleClick).toHaveBeenCalled();
+    });
+
+    it('formBtn check button state is disabled when textbox is empty', () => {
+      const { getByTestId } = render(
+        <Button
+          type="button"
+          handleClick={handleClick}
+          testid="formBtn"
+          isDisabled
+        />,
+      );
+      const btn = getByTestId('formBtn');
+
+      expect(btn).toBeDisabled();
     });
   });
 });
