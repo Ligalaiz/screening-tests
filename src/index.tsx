a4e66cafec12cbfdde0f4cf462e86e96bf2e088f
Ligalaiz <slavakuz88@gmail.com>
2022-04-20 14:06:25 +0300

    refactor: RF-1 import router and renderApp utils

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import { render } from 'react-dom';
 import { Global } from '@emotion/react';
 import { App } from '@src/App';
 import { globalStyle } from '@src/styles';
+import { RenderAppUtils } from '@utils/renderApp.utils';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 render(
-  <div className="container">
-    <Global styles={globalStyle} />
-    <App />
-  </div>,
+  <Router>
+    <div className="container">
+      <Global styles={globalStyle} />
+      <RenderAppUtils />
+      <App />
+    </div>
+  </Router>,
   document.getElementById('root'),
 );
