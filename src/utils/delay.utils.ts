29d1dacb0d8ed1c28bf876646f09e0dfee80b86b
Ligalaiz <slavakuz88@gmail.com>
2022-04-20 14:02:50 +0300

    refactor: RF-1 remove delay utils

--- src/utils/delay.utils.ts
+++
@@ -1,9 +0,0 @@
-const delay = (ms: number, callback?: jest.Mock<any, any>) =>
-  new Promise<void>((res) =>
-    setTimeout(() => {
-      res();
-      if (typeof callback === 'function') callback();
-    }, ms),
-  );
-
-export { delay };
