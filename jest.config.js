931521339f6570d51300a46a30fe9170f4db7122
Ligalaiz <slavakuz88@gmail.com>
2022-04-22 09:18:06 +0300

    build: RF-1 add alias for jest

--- jest.config.js
+++ jest.config.js
@@ -31,6 +31,7 @@ module.exports = {
     '^@modules(.*)$': '<rootDir>/src/modules$1',
     '^@utils(.*)$': '<rootDir>/src/utils$1',
     '^@assets(.*)$': '<rootDir>/src/assets$1',
+    '^@screens(.*)$': '<rootDir>/src/screens$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/internals/mocks/fileMock.js',
     '\\.(css|scss)$': '<rootDir>/src/internals/mocks/styleMock.js',
