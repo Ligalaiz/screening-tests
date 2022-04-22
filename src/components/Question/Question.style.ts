bc521e709e608b095fe273d2f79a0b71e67e8296
Ligalaiz <slavakuz88@gmail.com>
2022-04-22 12:36:16 +0300

    style: RF-1 change Question styles

--- src/components/Question/Question.style.ts
+++ src/components/Question/Question.style.ts
@@ -2,15 +2,17 @@ import { css } from '@emotion/react';
 import { color } from '@src/styles';
 
 export const question = css`
-  color: ${color['gray-color-light']};
-  font-size: 1.7rem;
-  line-height: 2rem;
   max-width: 374px;
+
+  font-size: 1.7rem;
+  color: ${color['gray-color-light']};
+  line-height: 2rem;
   text-align: center;
 `;
 
 export const loginLink = css`
   margin-left: 5px;
+
   font-family: 'SF Compact Display';
   font-weight: 600;
   font-size: 1.7rem;
