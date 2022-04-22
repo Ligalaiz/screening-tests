e1b317efb99d3f827aeab46bea27048aefb730a5
Ligalaiz <slavakuz88@gmail.com>
2022-04-22 09:16:25 +0300

    test: RF-1 change Question test

--- src/components/Question/Question.test.tsx
+++ src/components/Question/Question.test.tsx
@@ -6,14 +6,14 @@ import React from 'react';
 const renderComponent = () => render(<Question question="question" link="link" />);
 
 describe('Question component', () => {
-  describe('Component not exist', () => {
-    it('formBtn check that the component not has been rendered #smoke', () => {
+  describe('Component exist', () => {
+    it('formBtn check that the component has been rendered #smoke', () => {
       const { queryByTestId } = renderComponent();
 
-      expect(queryByTestId('formQuestion')).not.toBeInTheDocument();
-      expect(queryByTestId('formQuestion')).not.toHaveTextContent('question');
-      expect(queryByTestId('formLink')).not.toBeInTheDocument();
-      expect(queryByTestId('formLink')).not.toHaveTextContent('link');
+      expect(queryByTestId('formQuestion')).toBeInTheDocument();
+      expect(queryByTestId('formQuestion')).toHaveTextContent('question');
+      expect(queryByTestId('formLink')).toBeInTheDocument();
+      expect(queryByTestId('formLink')).toHaveTextContent('link');
     });
   });
 });
